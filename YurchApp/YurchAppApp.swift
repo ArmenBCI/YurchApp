23743413caf2b53adb6a5fc756b9576865cc02b8
ArmenBCI <125582643+ArmenBCI@users.noreply.github.com>
2023-09-19 19:15:55 +0400

    firstAdd

---
+++ YurchApp/YurchAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  YurchAppApp.swift
+//  YurchApp
+//
+//  Created by Armen Sukiasyan on 19.09.23.
+//
+
+import SwiftUI
+
+@main
+struct YurchAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
